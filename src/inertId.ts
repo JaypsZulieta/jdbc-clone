1ee17d6cda1cef1559ece556fd5b3c2e3a416226
Jaypee P. Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-06-22 18:19:58 +0800

    feat: Add InsertId class.

---
+++ src/inertId.ts
@@ -0,0 +1,30 @@
+import { z as Zod } from "zod";
+import { DataValidationSQLException } from "./exceptions";
+
+export class InsertId {
+  private insertId: any;
+
+  public constructor(insertId: any) {
+    this.insertId = insertId;
+  }
+
+  public async getAsString(): Promise<string> {
+    if (await this.isNotAString(this.insertId))
+      throw new DataValidationSQLException("The insert id is not a string.");
+    return this.insertId as string;
+  }
+
+  public async getAsNumber(): Promise<number> {
+    if (await this.isNotANumber(this.insertId))
+      throw new DataValidationSQLException("The insert id is not a number");
+    return this.insertId as number;
+  }
+
+  private async isNotAString(data: any): Promise<boolean> {
+    return !(await Zod.string().safeParseAsync(data)).success;
+  }
+
+  private async isNotANumber(data: any): Promise<boolean> {
+    return !(await Zod.number().safeParseAsync(data)).success;
+  }
+}
